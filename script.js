339c785e39cfae072052cdd7f8c09ba13ea43970
felipericardo12 <felipeeemoreiraaa12@gmail.com>
2026-03-26 11:28:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ alert(`Voce terá ${idade + 18} daqui 18 anos!`);
 
 let primeiron = parseInt(prompt('Digite um numero'));
 let segundon = parseInt(prompt('Digite outro numero'));
-let soma = primeiron * segundon
+let soma = primeiron + segundon
 let multi = primeiron * segundon
  alert(`Mostre o resultado ${soma} da soma`);
  alert(`Mostre o resultado ${multi} da multi`);
