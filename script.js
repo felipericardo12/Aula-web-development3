1c66fc0be45ce7f31dd6df44faf26bc0a6d70a10
Felipe <felipeeemoreiraaa12@gmail.com>
2026-03-26 11:19:45 -0300

    chore :iniciando novo site js

---
+++ script.js
@@ -0,0 +1,16 @@
+alert('Bem vindo!');
+let nome = prompt('Digite seu nome:');
+alert(`Ola, ${nome}!`);
+
+
+let idade = parseInt(prompt('Digite a idade'));
+alert(`Voce terá ${idade + 18} daqui 18 anos!`);
+
+//atividade 
+
+let primeiron = parseInt(prompt('Digite um numero'));
+let segundon = parseInt(prompt('Digite outro numero'));
+let soma = primeiron * segundon
+let multi = primeiron * segundon
+ alert(`Mostre o resultado ${soma} da soma`);
+ alert(`Mostre o resultado ${multi} da multi`);
